2f37385e2ebc14440520cb9f19c9815d325e250e
Omar Vazquez <code2success@Omars-MacBook-Pro.local>
2018-01-24 01:43:36 -0800

    More updates

---
+++ app/models/user.js
@@ -0,0 +1,26 @@
+var mongoose	= require('mongoose')
+var Schema		= mongoose.Schema
+
+// User Schema
+var UserSchema	= new Schema({
+	name: {
+		first: String,
+		last: String
+	},
+	email: {
+		type: String,
+		required: true,
+		index: { unique: true }
+	},
+	password: {
+		type: String,
+		required: true,
+		select: false
+	},
+	inseam: {
+		type: Number,
+		required: true
+	}
+})
+
+module.exports = mongoose.model('User', UserSchema)
