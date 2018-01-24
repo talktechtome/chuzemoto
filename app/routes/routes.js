f5aa863357c056c9b268768317e214bbbb66c30c
Omar Vazquez <code2success@Omars-MacBook-Pro.local>
2018-01-24 01:39:51 -0800

    Added new routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,3 +1,5 @@
+var User 	= require('../models/user.js')
+
 module.exports = function(app, express) {
 
 	// HOME PAGE - GET www.nameofapp.com/
@@ -6,9 +8,81 @@ module.exports = function(app, express) {
 		res.render("index.ejs")
 	})
 
-	// SIGNUP PAGE - GET nameofapp.com/signup
+	// SIGNUP PAGE
 
+	// GET chuzemoto.herokuapp.com/signup
 	app.get('/signup', function(req, res) {
 		res.render("signup.ejs")
+	})
+
+	// POST chuzemoto.herokuapp.com/signup
+	app.post('/signup', function(req, res) {
+
+		var user = new User()
+
+		user.name.first = req.body.fname
+		user.name.last = req.body.lname
+		user.email = req.body.email
+		user.password = req.body.password
+		user.inseam = req.body.inseam
+
+		user.save(function(err) {
+			if (err) {
+				// duplicate entry
+				if (err.code == 11000)
+					return res.json({ 
+						success: false, 
+						message: 'A user with email already exists.'
+					})
+				else
+					return res.send(err)
+			}
+
+			// return a sucess message
+			res.json({ message: 'User created!'})
+		})
+	})
+
+	// LOGIN PAGE
+
+	// GET - chuzemoto.herokuapp.com/login
+	app.get('/login', function(req, res) {
+		res.render("login.ejs")
+	})
+
+	// POST - chuzemoto.herokuapp.com/login
+	app.post('/login', function(req, res) {
+
+		// find the user (or at least check if exists)
+		User.findOne({
+			email: req.body.email
+		}).select('password').exec(function(err, user) {
+
+			if (err) throw err;
+
+			// no user with that email is found
+			if (!user) {
+				res.json({
+					success: false,
+					message: 'No user with that email exists.'
+				})
+			}
+			// if user is found
+			else if (user) {
+
+				// check if passwords match
+				// if not matched, give error
+				if (!(user.password == req.body.password)) {
+					res.json({
+						success: false,
+						message: 'Sorry, wrong password.'
+					})
+				}
+
+				// if matched, redirect to dashboard page.
+				res.render('dashboard.ejs')
+
+			}
+		})
 	})
 }
